45fd87f43801500049c28510afe075fd1d745762
Evelyn Sjafii <evelynsj98@gmail.com>
2019-05-26 20:45:17 -0700

    More initialization

--- main.cpp
+++ main.cpp
@@ -23,7 +23,14 @@ struct Host {
 // TODO: Do we need a global event list?
 
 /* TODO: global variables for processing */
-int NUM_HOSTS = 1;
+int NUM_HOSTS = 1; // variable
+int T = 400; // variable. TODO: figure out how much this is?
+double ARRIVAL_RATE = 0.01; // variable lambda
+double SIFS = 0.00005;
+double DIFS = 0.0001;
+double SENSE = 0.00001;
+double curr_time;
+double service_rate; // mu. TODO: is this needed?
 
 /* global variables for statistics */
 double transmitted_bytes;
@@ -35,6 +42,9 @@ double throughput;
 double avg_network_delay;
 
 void initialize() {
+
+    /* Initialize processing variables */
+    curr_time = 0.0;
 
     /* Initialize statistical variables */
     transmitted_bytes = 0.0;
