dc31de218c472f4bbc94350697a05af80f8b9183
Evelyn Sjafii <evelynsj98@gmail.com>
2019-05-29 00:58:45 -0700

    Global variables and data structures

--- cleanup.cpp
+++ cleanup.cpp
@@ -23,5 +23,35 @@ struct Event {
     Frame* fr;
 };
 
+/* Global variables for processing */
+double current_time;
+bool channel_idle;
+
+/* Constant variables for processing */
+const int NUM_HOSTS = 2;
+const int T = 5;
+const double ARRIVAL_RATE = 0.01; // lambda
+const double MAX_FRAME = 1544;
+const double ACK_FRAME = 64;
+const double CHANNEL_CAP = 10000000; // 1 Mbps = 10^6 bits/sec 
+const double SIFS = 0.00005;
+const double DIFS = 0.0001;
+const double SENSE = 0.00001;
+
+/* Global variables for statistics */
+double transmitted_bytes;
+double total_time;
+double total_delay;
+
+/* Output statistics */
+double throughput;
+double avg_network_delay;
+
+/* Data structures */
+Host* hosts[NUM_HOSTS];
+Event* GELhead;
+Event* GELtail;
+int GELsize;
+
 int main() {
 }
